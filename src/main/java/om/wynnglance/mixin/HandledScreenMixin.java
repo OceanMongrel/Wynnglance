e1f706618a23b8528975fa5ff599da745f627641
Incompleteusern <58920010+Incompleteusern@users.noreply.github.com>
2022-01-24 07:50:09 -0700

    optimize

--- src/main/java/om/wynnglance/mixin/HandledScreenMixin.java
+++ src/main/java/om/wynnglance/mixin/HandledScreenMixin.java
@@ -20,52 +20,72 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HandledScreen.class)
 public abstract class HandledScreenMixin {
+    private static final RarityMatcher[] rarityMatchers = new RarityMatcher[8];
 
-    private Identifier TEXTURE = new Identifier("wynnglance", "textures/aura.png");
-    private int rarity = 0;
-
-    private int detectRarity(String display, String ... match) {
-        for (String i : match) {
-            if (display.contains(i)
-                && !display.contains("§6Ingredient")
-                && !display.contains("§7Crafting")) {
-                return 1;
-            }
-        }
-        return 0;
-    }
-
-    private float getChannel(int channel) {
-        return WynnglanceClient.rarityColours[rarity - 1][channel];
+    static {
+        rarityMatchers[0] = new RarityMatcher(0.0F, 0.66666F, 0.66666F, "§3Crafted");
+        rarityMatchers[1] = new RarityMatcher(0.33333F, 1.0F, 0.33333F, "§aSet");
+        rarityMatchers[2] = new RarityMatcher(0.66666F, 0.0F, 0.66666F, "§5Mythic");
+        rarityMatchers[3] = new RarityMatcher(1.0F, 0.33333F, 0.33333F, "§cFabled");
+        rarityMatchers[4] = new RarityMatcher(0.33333F, 1.0F, 1.0F, "§bLegendary", "§b✫✫✫");
+        rarityMatchers[5] = new RarityMatcher(1.0F, 0.33333F, 1.0F, "§dRare", "§d✫✫§8✫");
+        rarityMatchers[6] = new RarityMatcher(1.0F, 1.0F, 0.33333F, "§eUnique", "§e✫§8✫✫");
+        rarityMatchers[7] = new RarityMatcher(1.0F, 1.0F, 1.0F, "§fNormal", "§8✫✫✫");
     }
 
     @Inject(method = "drawSlot", at = @At(value = "HEAD"))
     private void drawSlotInject(MatrixStack matrices, Slot slot, CallbackInfo ci) {
         ItemStack stack = slot.getStack();
-        if (!stack.isEmpty()) {
-            NbtCompound nbt = stack.getNbt();
-            if (nbt != null) {
-                if (nbt.contains("display")) {
-                    String nbtDisplay = nbt.get("display").asString();
-                    rarity =
-                        Math.max(detectRarity(nbtDisplay, "§fNormal", "§8✫✫✫") * 1,
-                        Math.max(detectRarity(nbtDisplay, "§eUnique", "§e✫§8✫✫") * 2,
-                        Math.max(detectRarity(nbtDisplay, "§dRare", "§d✫✫§8✫") * 3,
-                        Math.max(detectRarity(nbtDisplay, "§bLegendary", "§b✫✫✫") * 4,
-                        Math.max(detectRarity(nbtDisplay, "§cFabled") * 5,
-                        Math.max(detectRarity(nbtDisplay, "§5Mythic") * 6,
-                        Math.max(detectRarity(nbtDisplay, "§aSet") * 7,
-                        detectRarity(nbtDisplay, "§3Crafted") * 8)))))));
+
+        if (stack.isEmpty()) return;
+
+        NbtCompound nbt = stack.getNbt();
+        if (nbt == null || !nbt.contains("display")) return;
+
+        String display = nbt.get("display").asString();
+
+        if (display.contains("§6Ingredient") || display.contains("§7Crafting")) return;
+
+        for (RarityMatcher rarityMatcher : rarityMatchers) {
+            if (rarityMatcher.canApply(display)) {
+                rarityMatcher.apply(display);
+                return;
+            }
+        }
+    }
+
+    public static class RarityMatcher {
+        private static final Identifier TEXTURE = new Identifier("wynnglance", "textures/aura.png");
+
+        private final String[] matches;
+        private final float r;
+        private final float g;
+        private final float b;
+
+        public RarityMatcher(float r, float g, float b, String... matches) {
+            this.matches = matches;
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+
+        public void apply(String display) {
+            RenderSystem.enableBlend();
+            RenderSystem.setShader(GameRenderer::getPositionTexShader);
+            RenderSystem.setShaderColor(r, g, b, 0.5F);
+            RenderSystem.setShaderTexture(0, TEXTURE);
+            DrawableHelper.drawTexture(matrices, slot.x - 1, slot.y - 1, 0, 0, 18, 18, 18, 18);
+            RenderSystem.disableBlend();
+        }
+
+        public boolean canApply(String display) {
+            for (String i : matches) {
+                if (display.contains(i)) {
+                    return true;
                 }
             }
-            if (rarity > 0) {
-                RenderSystem.enableBlend();
-                RenderSystem.setShader(GameRenderer::getPositionTexShader);
-                RenderSystem.setShaderColor(getChannel(0), getChannel(1), getChannel(2), 0.5F);
-                RenderSystem.setShaderTexture(0, TEXTURE);
-                DrawableHelper.drawTexture(matrices, slot.x - 1, slot.y - 1, 0, 0, 18, 18, 18, 18);
-                RenderSystem.disableBlend();
-            }
+
+            return false;
         }
     }
 }
